36b52b4c0c0936707213ca4a31535aa04d6f094d
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-15 16:00:48 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -5,7 +5,9 @@ def my_find(collection)
   i = 0 
   
   while i < collection.length 
-    yield(collection[i])
+    if yield(collection[i])
+      return collection[i]
+    end
     i += 1
   end 
   
